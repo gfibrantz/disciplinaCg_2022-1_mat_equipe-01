351558d06caf9a50e110ae50c5b89f8d19b8038b
tatalb <55159053+tatalb@users.noreply.github.com>
2022-06-30 09:14:51 -0300

    alterando nome n4

--- unidade_4/CG_N2/Mundo.cs
+++ unidade_4/CG_N2/Mundo.cs
@@ -603,7 +603,7 @@ namespace gcgcg
         static void Main(string[] args)
         {
             Mundo window = Mundo.GetInstance(600, 600);
-            window.Title = "CG_N2";
+            window.Title = "CG_N4";
             window.Run(1.0 / 60.0);
         }
     }
